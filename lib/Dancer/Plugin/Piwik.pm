54aedede4db3a6220b911278efeedde4f2313330
Stefan Hornburg (Racke) <racke@linuxia.de>
2016-11-21 11:44:29 +0100

    Bump up version number.

--- lib/Dancer/Plugin/Piwik.pm
+++ lib/Dancer/Plugin/Piwik.pm
@@ -12,11 +12,11 @@ Dancer::Plugin::Piwik - Generate JS code for Piwik
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 # on loading, spam the message if disabled
 
